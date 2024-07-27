4ca249ea929a683fdd639d61f4b1a64a00c8a35f
Brandon Lepore <bjt.lepore@gmail.com>
2024-07-27 16:50:53 +0930

    feat: :sparkles: add main textarea field

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,25 @@
-import { component$ } from '@builder.io/qwik';
+import { $, component$, useSignal } from '@builder.io/qwik';
+
+import type { PropsOf, QRL } from '@builder.io/qwik';
 
 export default component$(() => {
-  return <h1>Welcome &#9996;</h1>;
+  const v = useSignal('');
+  const text = 'text';
+
+  const onInput: QRL<PropsOf<'textarea'>['onInput$']> = $((_event, { value }) => {
+    v.value = value.replace(/\n/g, ' ');
+  });
+
+  return (
+    <textarea
+      name={text}
+      id={text}
+      title={text}
+      class="flex h-full w-full flex-grow"
+      placeholder="Lorem, ipsum..."
+      fetchPriority="high"
+      value={v.value}
+      onInput$={onInput}
+    ></textarea>
+  );
 });
