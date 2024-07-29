2ef25787164c5fa802b78e13e6516bf1fa58161e
fatherbrennan <bjt.lepore@gmail.com>
2024-07-29 18:19:28 +0930

    ci: :construction_worker: test

--- src/components/Main/index.tsx
+++
@@ -1,9 +0,0 @@
-import { component$, Slot } from '@builder.io/qwik';
-
-export const Main = component$(() => {
-  return (
-    <main class="h-full overflow-auto bg-paper p-2">
-      <Slot />
-    </main>
-  );
-});
