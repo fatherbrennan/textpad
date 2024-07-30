c85d836826b38c27973d047f243227e055bf7c51
fatherbrennan <bjt.lepore@gmail.com>
2024-07-30 18:00:18 +0930

    build: :construction: account for /textpad pathname in build and local dev

--- adapters/static/vite.config.ts
+++ adapters/static/vite.config.ts
@@ -13,7 +13,7 @@ export default extendConfig(baseConfig, () => {
     },
     plugins: [
       staticAdapter({
-        origin: 'https://fatherbrennan.github.com/textpad',
+        origin: 'https://fatherbrennan.github.com',
       }),
     ],
   };
